008d4e6a1af94772ac7930d63bcc30a6fb9a390a
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-01-07 23:17:11 +0100

    feat: add pharmacy schema with properties for management and services

---
+++ backend/src/schemas/pharmacy.schema.ts
@@ -0,0 +1,37 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { Address, AddressSchema } from './address.schema';
+
+@Schema({ timestamps: true })
+export class Pharmacy extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ type: AddressSchema, required: true })
+  address: Address;
+
+  @Prop({ required: true })
+  phone: string;
+
+  @Prop()
+  email: string;
+
+  @Prop([
+    {
+      open: { type: String, required: true },
+      close: { type: String, required: true },
+    },
+  ])
+  openingHours: Array<{ open: string; close: string }>;
+
+  @Prop([String])
+  services: string[];
+
+  @Prop([String])
+  images: string[];
+
+  @Prop({ default: false })
+  isOnGard: boolean;
+}
+
+export const PharmacySchema = SchemaFactory.createForClass(Pharmacy);
