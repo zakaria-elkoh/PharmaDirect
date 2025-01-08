179259219a6e3b4485b08bb3689bf3063af26afe
Bilal Ezzaim <bilalzaim499@gmail.com>
2025-01-08 10:01:58 +0100

    Création du DTO, module d'authentification, schéma et configuration de la connexion à la base de données

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/pharmaDirect'),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
