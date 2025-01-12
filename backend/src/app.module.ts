53072020ad09407c570b73c1c0d1ef12db372022
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-01-12 15:29:06 +0100

    feat: add Pharmacy module to AppModule and configure MongoDB connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SchemasModule } from './schemas/schemas.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PharmacyModule } from './pharmacy/pharmacy.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/pharmadirect'),
+    PharmacyModule,
+  ],
 })
 export class AppModule {}
