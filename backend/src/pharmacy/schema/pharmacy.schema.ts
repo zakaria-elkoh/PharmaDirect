30599f5e33007603ab2a2579d8aa4e004141a2bc
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-01-12 15:30:26 +0100

    feat: add Pharmacy schema with properties for name, address, phone, email, opening hours, services, images, and isOnGard flag

---
+++ backend/src/pharmacy/schema/pharmacy.schema.ts
@@ -0,0 +1,39 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { Address, AddressSchema } from '../../schemas/address.schema';
+
+@Schema({ timestamps: true })
+export class Pharmacy extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ type: AddressSchema, required: true })
+  address: Address;
+
+  @Prop({ required: true })
+  phone: string;
+
+  @Prop()
+  email: string;
+
+  @Prop([
+    {
+      day: { type: String, required: true },
+      open: { type: String, required: true },
+      close: { type: String, required: true },
+    },
+  ])
+  openingHours: Array<{ day: string; open: string; close: string }>;
+
+  @Prop([String])
+  services: string[];
+
+  @Prop([String])
+  images: string[];
+
+  @Prop({ default: false })
+  isOnGard: boolean;
+}
+
+export const PharmacySchema = SchemaFactory.createForClass(Pharmacy);
+PharmacySchema.index({ 'address.location': '2dsphere' });
